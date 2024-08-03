700e22b096337a64bc7125d5f15e2a83d46e4f4e
Jiacheng He <93626337+HarrryHe@users.noreply.github.com>
2024-08-03 00:10:49 -0400

    completed music cog implementation

--- cogs/music.py
+++ cogs/music.py
@@ -14,10 +14,6 @@ ytdl_format_options = {
     'noplaylist': True,
 }
 
-"""ffmpeg_options = {
-    'options': '-vn'
-}"""
-
 #create youtubeDL instance
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
@@ -27,6 +23,30 @@ class music(Cog_Extension):
         self.queues = {}        
         self.current = None
         self.voice_clients = {}
+
+    #add songs into queue
+    def add_to_queue(self, guild_id, song):
+        if guild_id not in self.queues:
+            self.queues[guild_id] = []
+        self.queues[guild_id].append(song)
+
+    #call_back function
+    async def play_next(self, ctx):
+
+        guild_id = ctx.guild.id
+
+        if guild_id not in self.queues or not self.queues[guild_id]:
+            return
+        
+        #pop the first song from queues' guild list
+        song = self.queues[guild_id].pop(0)
+        url = song['url']
+        title = song['title']
+
+        #let the voice client play the song, and after it finished call the function itself until theres no song left
+        source = discord.FFmpegPCMAudio(url, options='-vn')
+        ctx.voice_client.play(source, after=lambda e: self.bot.loop.create_task(self.play_next(ctx)))
+        await ctx.send(f'Now playing: {title}')
     
     #invoke bot into voice channel
     @commands.command(name="join_vc")
@@ -46,6 +66,7 @@ class music(Cog_Extension):
     async def leave(self, ctx):
         if ctx.guild.id in self.voice_clients:
             await self.voice_clients[ctx.guild.id].disconnect()
+            #delete the queue from [ctx] guild
             del self.voice_clients[ctx.guild.id]
 
     @commands.command(name="play")
@@ -70,11 +91,15 @@ class music(Cog_Extension):
             if 'entries' in data:
                 data = data['entries'][0]
 
-            filename = data['url']
+            url = data['url']
             title = data['title']
-            source = discord.FFmpegPCMAudio(filename, options='-vn')
-            voice_client.play(source)
-            await ctx.send(f'--- now playing {title} ---')
+            song = {'url': url, 'title': title}
+            self.add_to_queue(ctx.guild.id, song)
+
+            await ctx.send(f'Added {title} to the queue.')
+
+            if not voice_client.is_playing() and not voice_client.is_paused():
+                await self.play_next(ctx)
 
     @commands.command(name="pause")
     async def pause(self, ctx):
@@ -102,6 +127,24 @@ class music(Cog_Extension):
         else:
             await ctx.send("The audio is not paused.", delete_after=5)
 
+    @commands.command(name="skip")
+    async def skip(self, ctx):
+        voice_client = self.voice_clients[ctx.guild.id]
+        if voice_client and voice_client.is_playing():
+            voice_client.stop()
+            await ctx.send("Skipped the current song.")
+        else:
+            await ctx.send("No audio is playing.", delete_after=5)
+
+    #show the current queue
+    @commands.command()
+    async def queue(self, ctx):
+        queue = self.queues[ctx.guild.id]
+        if queue:
+            queue_str = '\n'.join([f"{idx + 1}. {song['title']}" for idx, song in enumerate(queue)])
+            await ctx.send(f"Current Queue: \n {queue_str}")
+        else:
+            await ctx.send("The queue is empty.", delete_after=5)
 
 
 async def setup(bot):
